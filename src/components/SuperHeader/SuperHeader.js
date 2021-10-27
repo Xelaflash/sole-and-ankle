54d3edfaefb1517815cedfe266d21d1f734d03ee
Alexandre Gros <xelaflash@gmail.com>
2021-10-27 09:19:12 -0400

    🚀 superheader styling

--- src/components/SuperHeader/SuperHeader.js
+++ src/components/SuperHeader/SuperHeader.js
@@ -7,29 +7,40 @@ import SearchInput from '../SearchInput';
 import UnstyledButton from '../UnstyledButton';
 import Icon from '../Icon';
 
-const SuperHeader = () => {
-  return (
-    <Wrapper>
-      <MarketingMessage>
-        Free shipping on domestic orders over $75!
-      </MarketingMessage>
+const SuperHeader = () => (
+  <Wrapper>
+    <MarketingMessage>
+      Free shipping on domestic orders over $75!
+    </MarketingMessage>
+    <FlexWrapper>
       <SearchInput />
       <HelpLink href="/help">Help</HelpLink>
       <UnstyledButton>
         <Icon id="shopping-bag" strokeWidth={1} />
       </UnstyledButton>
-    </Wrapper>
-  );
-};
+    </FlexWrapper>
+  </Wrapper>
+);
 
 const Wrapper = styled.div`
   font-size: 0.875rem;
+  height: 40px;
   color: ${COLORS.gray[300]};
   background-color: ${COLORS.gray[900]};
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `;
 
 const MarketingMessage = styled.span`
   color: ${COLORS.white};
+  padding-left: 32px;
+`;
+
+const FlexWrapper = styled.div`
+  display: flex;
+  padding-right: 18px;
+  gap: 27px;
 `;
 
 const HelpLink = styled.a`
